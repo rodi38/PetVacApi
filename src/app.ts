8e5d0a37b00a2dccef0330fca05e9c4477be7d36
Rodrigo Rocha <rodrigorochaed1@gmail.com>
2024-10-27 19:56:46 -0300

    chore: port for render

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,6 @@ dotenv.config();
 import Fastify from "fastify";
 import jwt from "@fastify/jwt";
 
-
 import { AppDataSource } from "./config/typeorm";
 import authRouter from "./routes/authRouter";
 import { indexRouter } from "./routes/indexRouter";
@@ -13,10 +12,11 @@ import petRouter from "./routes/petRouter";
 import vaccineRouter from "./routes/vaccineRouter";
 
 const app = Fastify();
+const PORT = Number(process.env.PORT) || 5000;
 
 app.register(jwt, {
-    secret: process.env.JWT_SECRET || "fallback_secret"
-  });
+	secret: process.env.JWT_SECRET || "fallback_secret",
+});
 
 AppDataSource.initialize()
 	.then(() => {
@@ -33,8 +33,8 @@ app.register(indexRouter);
 
 const start = async () => {
 	try {
-		await app.listen({ port: 3000 });
-		console.log("Server started on http://localhost:3000");
+		await app.listen({ port: PORT, host: "0.0.0.0" });
+		console.log(`Server started on http://localhost: ${PORT}`);
 	} catch (err) {
 		app.log.error(err);
 		process.exit(1);
